23e6091a800b89c5b9a487d03c8617850fe15769
Rastislav Lamos <lamos.rasto@gmail.com>
2015-01-12 11:22:54 +0100

    Add support for  in array

--- includes/actions/class-lbh-actions-frontend.php
+++ includes/actions/class-lbh-actions-frontend.php
@@ -6,17 +6,16 @@
 class LBH_Actions_Frontend extends Lamosty_Actions {
 	protected $id = 'frontend';
 
-	private function hide_cat_from_homepage( $cat_ID ) {
+	private function hide_cat_from_homepage( $cat_IDs ) {
 		$this->dispatcher->dispatch( array(
 			"action_type" => "hide_cat_from_homepage",
-			"cat_ID"      => $cat_ID
+			"cat_IDs"      => $cat_IDs
 		) );
 
 	}
 
 	public function init_actions() {
-		$this->hide_cat_from_homepage(56);
-		$this->hide_cat_from_homepage(55);
+		$this->hide_cat_from_homepage(array(55, 56));
 	}
 
 }
--- includes/stores/class-lbh-store-frontend.php
+++ includes/stores/class-lbh-store-frontend.php
@@ -6,15 +6,26 @@
 class LBH_Store_Frontend extends Lamosty_Store {
 	protected $id = 'frontend';
 
-	public function hide_cat_from_homepage( $cat_ID ) {
-		$this->add_single_data( 'cat_ID', $cat_ID );
+	public function hide_cat_from_homepage( $cat_IDs ) {
+		$this->add_single_data( 'cat_ID', $cat_IDs );
 
 		add_action( 'pre_get_posts', array( $this, 'action_hide_cat_from_homepage' ) );
 	}
 
 	public function action_hide_cat_from_homepage( WP_Query $query ) {
+		$cat_IDs                 = $this->get_single_data( 'cat_IDs' );
+		$cat_IDs_excluded_string = '';
+
+		for ( $i = 0; $i < count( $cat_IDs ); $i ++ ) {
+			$cat_IDs_excluded_string .= "-{$cat_IDs[$i]}";
+
+			if ( $i + 1 < count( $cat_IDs ) ) {
+				$cat_IDs_excluded_string .= ",";
+			}
+		}
+
 		if ( $query->is_home() && $query->is_main_query() ) {
-			$query->set( 'cat', '-' . $this->get_single_data( 'cat_ID' ) );
+			$query->set( 'cat', $cat_IDs_excluded_string );
 		}
 	}
 }
